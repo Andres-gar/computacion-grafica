380ebd71d4fa7e630a43b5ccebc4a20be026e2a6
Andrés García <andres.garcia.gaviria@correounivalle.edu.co>
2021-03-04 19:06:38 -0500

    Capitulos del 4.1 al 4.3

---
+++ Capitulo 4/4.2. KeyboardSupport/Site Root/src/Engine/Core/Engine_Core.js
@@ -0,0 +1,46 @@
+"use strict"; // Operate in Strict mode
+var gEngine = gEngine || {};
+// inicializa la variable mientras se aseura que no esta definida
+
+gEngine.Core = (function () {
+    // Variable privada, define el contexto 
+    var mGL = null;
+    // Accesso al contexto WebGL
+    var getGL = function () { return mGL; };
+    // Contiene las funciones y variable que seran accesibles
+    var mPublic = {
+        getGL: getGL
+    };
+    // inicializa el WebGL, el vertex buffer y compila los shaders
+    var _initializeWebGL = function (htmlCanvasID) {
+        var canvas = document.getElementById(htmlCanvasID);
+
+        mGL = canvas.getContext("webgl") ||
+            canvas.getContext("experimental-webgl");
+
+        if (mGL === null) {
+            document.write("<br><b>WebGL is not supported!</b>");
+        }
+    };
+
+    // initialize all of the EngineCore components
+    var initializeEngineCore = function (htmlCanvasID) {
+        _initializeWebGL(htmlCanvasID);
+        gEngine.VertexBuffer.initialize();
+        gEngine.Input.initialize();
+    };
+
+    // Limpia el area de trabajo y dibuja un cuadrado
+    var clearCanvas = function (color) {
+        mGL.clearColor(color[0], color[1], color[2], color[3]); // setea el color 
+        mGL.clear(mGL.COLOR_BUFFER_BIT); // limpia el color previo y setea
+    };
+
+    var mPublic = {
+        getGL: getGL,
+        initializeEngineCore: initializeEngineCore,
+        clearCanvas: clearCanvas
+    };
+    return mPublic;
+}());
+
---
+++ Capitulo 4/4.2. KeyboardSupport/Site Root/src/GLSLShaders/SimpleVS.glsl
@@ -0,0 +1,7 @@
+attribute vec3 aSquareVertexPosition;
+uniform mat4 uModelTransform; 
+uniform mat4 uViewProjTransform;
+
+void main(void) {
+    gl_Position = uViewProjTransform * uModelTransform * vec4(aSquareVertexPosition, 1.0); 
+}
